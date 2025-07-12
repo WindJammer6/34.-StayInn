ac6876169eb062623e4aebfc29f81404ce7a06f4
aaj1510 <aaj1510@gmail.com>
2025-07-12 17:53:02 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className='min-h-[70vh]'>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+        </Routes>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
