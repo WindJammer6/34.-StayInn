35eb762f6c86e260a3c9acd5965abe4f2da42c4a
Kridw4n <165155009+Kridw4n@users.noreply.github.com>
2025-07-13 21:52:58 +0800

    Changed ahref to Link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home';
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Hotels from "./pages/Hotels";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <div className='min-h-[70vh]'>
+      <div className="min-h-[70vh]">
         <Routes>
-            <Route path='/' element={<Home/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<Hotels />} />
         </Routes>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/pages/Hotels.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Hotels = () => {
+  return <div className="mt-24">List of Hotels</div>;
+};
+
+export default Hotels;
