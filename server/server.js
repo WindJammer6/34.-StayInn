62b240263936d7a4d59275405460b7a737ac6e42
furrbeebs <phoebel0904@gmail.com>
2025-07-14 10:28:00 +0800

    server.js - added API dummy data etc (check code comments)

--- server/server.js
+++ server/server.js
@@ -5,9 +5,23 @@ const axios = require("axios");
 
 const corsOptions = {
   origin: ["http://localhost:5173"],
+  credentials: true, // for clerk token later
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: [  // for clerk token
+    'Content-Type',
+    'Authorisation'
+  ]
 };
 
 app.use(cors(corsOptions));
+app.use(express.json());
+
+// middleware for when verifying clerk token later on
+/*
+const { ClerkExpressRequireAuth } = require('@clerk/clerk-sdk-node');
+
+app.use('/api/protected', ClerkExpressRequireAuth());
+*/
 
 app.get("/api/hotels/:id/prices", async (req, res) => {
   const hotelId = req.params.id;
@@ -31,6 +45,29 @@ app.get("/api/hotels/:id/prices", async (req, res) => {
     res.status(500).json({ error: "Failed to fetch room data" });
   }
 });
+
+// search API with dummy data
+const hotels = [
+  {name: "HolidayInn", city: "Malaysia", price: "$111/night"},
+  {name: "Hotel81", city: "Singapore", price: "$81/night"},
+  {name: "Ascott", city: "Thailand", price: "$200/night"}
+];
+
+app.post("/api/search-hotels", (req, res) => {
+  const {destination} = req.body;
+
+  // error handling
+  if (!destination){
+    return res.status(400).json({error: "Destination required"});
+  }
+
+  // exact matching for search
+  // if want to use partical matching: h => h.city.toLowerCase() .includes(destination.toLowerCase())
+  const filtered = hotels.filter(h => h.city.toLowerCase() === destination.toLowerCase());
+  res.json(filtered);
+});
+
+// to include app.get for clerk authentication route?
 
 app.listen(8080, () => {
   console.log("Server started on port 8080");
